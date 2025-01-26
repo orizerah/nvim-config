39175fbaa87af5310e19ae11f3ad834dbdeb38cb
Ori <ori.zerah@any.io>
2025-01-26 21:42:24 +0200

    renamed kickstart folder to ori

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -1,62 +1,18 @@
 require('lazy').setup({
-  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+  'tpope/vim-sleuth',
 
-  require 'kickstart/plugins/gitsigns',
-
-  require 'kickstart/plugins/which-key',
-
-  require 'kickstart/plugins/telescope',
-
-  require 'kickstart/plugins/lspconfig',
-
-  require 'kickstart/plugins/conform',
-
-  require 'kickstart/plugins/cmp',
-
-  require 'kickstart/plugins/tokyonight',
-
-  require 'kickstart/plugins/todo-comments',
-
-  require 'kickstart/plugins/mini',
-
-  require 'kickstart/plugins/treesitter',
-
-  require 'kickstart/plugins/hop',
-
-  require 'kickstart/plugins/avante',
-
-  require 'kickstart/plugins/alpha',
-
-  -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
-  --    This is the easiest way to modularize your config.
-  --
-  --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  { import = 'custom.plugins' },
-  --
-  -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
-  -- Or use telescope!
-  -- In normal mode type `<space>sh` then write `lazy.nvim-plugin`
-  -- you can continue same window with `<space>sr` which resumes last telescope search
-}, {
-  ui = {
-    -- If you are using a Nerd Font: set icons to an empty table which will use the
-    -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
-    icons = vim.g.have_nerd_font and {} or {
-      cmd = '⌘',
-      config = '🛠',
-      event = '📅',
-      ft = '📂',
-      init = '⚙',
-      keys = '🗝',
-      plugin = '🔌',
-      runtime = '💻',
-      require = '🌙',
-      source = '📄',
-      start = '🚀',
-      task = '📌',
-      lazy = '💤 ',
-    },
-  },
-})
-
--- vim: ts=2 sts=2 sw=2 et
+  require 'ori/plugins/gitsigns',
+  require 'ori/plugins/which-key',
+  require 'ori/plugins/telescope',
+  require 'ori/plugins/lspconfig',
+  require 'ori/plugins/conform',
+  require 'ori/plugins/cmp',
+  require 'ori/plugins/tokyonight',
+  require 'ori/plugins/todo-comments',
+  require 'ori/plugins/mini',
+  require 'ori/plugins/treesitter',
+  require 'ori/plugins/hop',
+  require 'ori/plugins/avante',
+  require 'ori/plugins/alpha',
+  require 'ori/plugins/commenter',
+}, {})
