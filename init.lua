9dcbb59e183b89856dcbe74e8eb2e8f4c97c4b94
Ori <ori.zerah@any.io>
2025-01-26 21:43:55 +0200

    removed README

--- init.lua
+++ init.lua
@@ -1,13 +1,8 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
-
--- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = true
+vim.opt.relativenumber = true
 
 require 'options'
 require 'keymaps'
 require 'lazy-bootstrap'
 require 'lazy-plugins'
-
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
