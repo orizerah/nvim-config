60409e9a7ca0d048426bcd01f860840b16568913
Ori <ori.zerah@any.io>
2025-01-26 21:58:55 +0200

    add typescript and go to conform

--- lua/ori/plugins/alpha.lua
+++ lua/ori/plugins/alpha.lua
@@ -1,15 +1,12 @@
 return {
   {
     'goolord/alpha-nvim',
-    -- dependencies = { 'echasnovski/mini.icons' },
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
       local alpha = require 'alpha'
       local dashboard = require 'alpha.themes.dashboard'
 
       local startify = require 'alpha.themes.startify'
-      -- available: devicons, mini, default is mini
-      -- if provider not loaded and enabled is true, it will try to use another provider
       startify.file_icons.provider = 'devicons'
       alpha.setup(startify.config)
       dashboard.section.header.val = {
@@ -23,7 +20,7 @@ return {
         '                             ',
       }
       dashboard.section.buttons.val = {
-        dashboard.button('f', '  > file browser', '<leader>fb'),
+        dashboard.button('f', '  > File Browser', '<leader>fb'),
         dashboard.button('r', '  > Recent', ':Telescope oldfiles<CR>'),
         dashboard.button('s', '  > Settings', ':e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>'),
         dashboard.button('q', '  > Quit NVIM', ':qa<CR>'),
